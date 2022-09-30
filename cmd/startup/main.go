b41fbbe88cb5bd99b4e24eec1a95f929ee51bae6
judegiordano <judegiordano@gmail.com>
2022-09-30 12:41:59 -0400

    Simple Fiber

--- cmd/startup/main.go
+++ cmd/startup/main.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/judegiordano/startup/pkg/logger"
 )
 
 func main() {
 	logger.Init()
-	logger.Info("sup")
+	app := fiber.New(fiber.Config{
+		ErrorHandler: ErrorHandler,
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+	})
+	app.Use(compress.New())
+	app.Use(recover.New())
+	app.Use(cors.New())
+	api := app.Group("/api")
+	api.Get("/dev", healthCheck)
+	app.Listen(fmt.Sprintf(":%v", 1234))
+}
+
+func healthCheck(c *fiber.Ctx) error {
+	type health struct {
+		Message string `json:"message"`
+	}
+	c.Status(418)
+	return c.JSON(health{
+		Message: "☕",
+	})
+}
+
+type ErrorResponse struct {
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
+
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+	message := err.Error()
+	logger.Error(err)
+	if e, ok := err.(*fiber.Error); ok {
+		code = e.Code
+		message = e.Message
+	}
+	ctx.Status(code).JSON(ErrorResponse{
+		Code:  code,
+		Error: message,
+	})
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/brianvoe/gofakeit/v6 v6.19.0
+	github.com/gofiber/fiber/v2 v2.38.1
 	github.com/golang-module/carbon v1.6.9
 	github.com/gosimple/slug v1.12.0
 	github.com/lucsky/cuid v1.2.1
@@ -13,16 +14,21 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/gobuffalo/envy v1.7.0 // indirect
 	github.com/gobuffalo/packd v0.3.0 // indirect
 	github.com/gobuffalo/packr v1.30.1 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/rogpeppe/go-internal v1.3.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 	golang.org/x/text v0.3.7 // indirect
